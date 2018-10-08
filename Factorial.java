0751c288379a7a7f104f9d74b84863d62f7ab447
Justin Sciglimpaglia <jnice23@optimum.net>
2018-10-08 00:10:38 -0400

    Factorial Code
    Java Code

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+
+public class Factorial {
+	
+	public static int fact(int n) {
+		
+		
+		if (n == 0)
+			return 1;
+		else 
+			return n * fact(n-1);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(fact(10));
+	}
+
+}
